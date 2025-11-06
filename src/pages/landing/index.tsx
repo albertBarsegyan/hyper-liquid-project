70f766b4a9549ef3926584cd40fab751a7555b96
Your Name <example@example.com>
2025-11-06 17:07:42 +0400

    fix: landing screen

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -17,11 +17,11 @@ const LandingPage: React.FC = () => {
 
   return (
     <div
-      className="min-h-screen relative flex flex-col"
+      className="relative flex flex-col"
       style={{ backgroundColor: '#0e1e27' }}
     >
       {/* Content Section */}
-      <div className="h-screen z-10 flex flex-col lg:flex-row lg:justify-between lg:flex-1">
+      <div className="min-h-screen z-10 flex flex-col lg:flex-row lg:justify-between lg:flex-1">
         <div className="hidden lg:flex flex-1 p-6">
           <div className="w-full h-full">
             <Carousel
